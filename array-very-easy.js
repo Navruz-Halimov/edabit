6661400b9c616d673bb5fe174d849d782e1b0306
Navruz <navruz.halimov.3999@gmail.com>
2023-09-27 15:52:50 +0500

    array very easy first problem

---
+++ array-very-easy.js
@@ -0,0 +1,19 @@
+
+//1.Problem
+//  Return the First Element in an Array
+// Create a function that takes an array containing only numbers and return the first element.
+
+// Examples
+// getFirstValue([1, 2, 3]) ➞ 1
+
+// getFirstValue([80, 5, 100]) ➞ 80
+
+// getFirstValue([-500, 0, 50]) ➞ -500
+// Notes
+// The first element in an array always has an index of 0.
+
+// solution
+// 1.function getFirstValue(arr) {
+// 	return arr[0]
+// }
+// 2. const getFirstValue=arr=>arr[0]
