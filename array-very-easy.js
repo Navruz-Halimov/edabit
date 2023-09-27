319e59d7ec52aaa40853b5c02622993ccd42a3a9
Navruz <navruz.halimov.3999@gmail.com>
2023-09-27 15:58:06 +0500

    2 problem solved with 3 solution

--- array-very-easy.js
+++ array-very-easy.js
@@ -1,5 +1,6 @@
 
-//1.Problem
+//#####1.Problem######
+
 //  Return the First Element in an Array
 // Create a function that takes an array containing only numbers and return the first element.
 
@@ -13,7 +14,28 @@
 // The first element in an array always has an index of 0.
 
 // solution
-// 1.function getFirstValue(arr) {
+// 1. function getFirstValue(arr) {
 // 	return arr[0]
 // }
 // 2. const getFirstValue=arr=>arr[0]
+
+
+// ######### 2problem##########
+
+// Pair Management
+// Given two arguments, return an array which contains these two arguments.
+
+// Examples
+// makePair(1, 2) ➞ [1, 2]
+
+// makePair(51, 21) ➞ [51, 21]
+
+// makePair(512124, 215) ➞ [512124, 215]
+
+// solution
+// 1. function makePair(num1, num2) {
+// 	return [num1,num2]
+// }
+// 2. const makePair=(num1,num2)=>[num1,num2]
+
+// 3. const makePair=(num1,num2)=> [...arguments]
