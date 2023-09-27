c25057bef76ec902a5f7398a42eda85810f5286f
Navruz <navruz.halimov.3999@gmail.com>
2023-09-27 16:46:31 +0500

    6 bugs fix problem solved

--- array-very-easy.js
+++ array-very-easy.js
@@ -115,3 +115,29 @@
 // 	arr.forEach(el=>reversedArray.unshift(el))
 // 	return reversedArray
 // }
+
+
+// #############6 problem##################
+// Fix the Bug: Simple Array Manipulation
+// Help fix all the bugs in the function incrementItems! It is intended to add 1 to every element in the array!
+
+// Examples
+// incrementItems([0, 1, 2, 3]) ➞ [1, 2, 3, 4]
+
+// incrementItems([2, 4, 6, 8]) ➞ [3, 5, 7, 9]
+
+// incrementItems([-1, -2, -3, -4]) ➞ [0, -1, -2, -3]
+
+
+// before
+// function incrementItems(arr) {
+// 	for (let i = 0; i < array.length; i++)
+// 		arr[i] === arr[i] + 1
+// 	return array
+// }
+// // solution
+// function incrementItems(arr) {
+// 	for (let i = 0; i < arr.length; i++)
+// 		arr[i] = arr[i] + 1
+// 	return arr
+// }
