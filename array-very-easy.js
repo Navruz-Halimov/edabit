fb1ccb87012abf8328030a028ea75ca525e9eddc
Navruz <navruz.halimov.3999@gmail.com>
2023-09-28 17:15:23 +0500

    28 problem solved with 2 solution

--- array-very-easy.js
+++ array-very-easy.js
@@ -708,3 +708,20 @@
 //   	return count;
 // }
 // 6. const length = s => [...s].reduce(a => a+1, 0);
+
+
+// ########### 28 #######################
+// Half, Quarter and Eighth
+// Create a function that takes a number and return an array of three numbers: half of the number, quarter of the number and an eighth of the number.
+
+// Examples
+// halfQuarterEighth(6) ➞ [3, 1.5, 0.75]
+
+// halfQuarterEighth(22) ➞ [11, 5.5, 2.75]
+
+// halfQuarterEighth(25) ➞ [12.5, 6.25, 3.125]
+
+// 1. function halfQuarterEighth(n) {
+//  return [n/2,n/4,n/8]	
+// }
+// 2. const halfQuarterEighth=n=>[n/2,n/4,n/8]
