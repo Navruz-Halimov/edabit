59613d2fa545437faf15a3c6b74270323e162fb5
Navruz <navruz.halimov.3999@gmail.com>
2023-09-28 08:49:14 +0500

    12 problem solved with 6 solution

--- array-very-easy.js
+++ array-very-easy.js
@@ -259,3 +259,29 @@
 //     }
 // }
 // }
+
+
+// ###########12 problem#################
+// Array Indexing
+// Given an index and an array, return the value of the array with the given index.
+
+// Examples
+// valueAt([1, 2, 3, 4, 5, 6], 10 / 2) ➞ 6
+
+// valueAt([1, 2, 3, 4, 5, 6], 8.0 / 2) ➞ 5
+
+// valueAt([1, 2, 3, 4], 6.535355314 / 2) ➞ 4
+
+// solution
+// 1. function valueAt(arr, i) {
+// 	return arr[Math.floor(i)]
+// }
+// 2. const valueAt=(arr,i)=> arr[Math.floor(i)]
+// 3. function valueAt(arr,i){
+//     return arr.find((el,index)=>index==(Math.floor(i)))
+// }
+// 4. const valueAt=(arr,i)=>arr.find((el,index)=>index==(Math.floor(i)))
+//  5. const valueAt= (arr,i)=>arr.filter((el,index)=>index==(Math.floor(i)))[0]
+//  6. function valueAt(arr,i) {
+//     return arr.filter((el,index)=>index==(Math.floor(i)))[0]
+//  }
