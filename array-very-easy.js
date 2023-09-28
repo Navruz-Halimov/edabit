58836ee2748a550ab126d913ad1f3d8306db7a73
Navruz <navruz.halimov.3999@gmail.com>
2023-09-28 09:08:23 +0500

    13 problem solved with  7 ways

--- array-very-easy.js
+++ array-very-easy.js
@@ -280,3 +280,35 @@
 //  13. function valueAt(arr,i) {
 //     return arr.filter((el,index)=>index==(Math.trunc(i)))[0]
 //  }
+
+// ################## 13 problem###########
+// Find the Index (Part 1)
+// Create a function that finds the index of a given item.
+
+// Examples
+// search([1, 5, 3], 5) ➞ 1
+
+// search([9, 8, 3], 3) ➞ 2
+
+// search([1, 2, 3], 4) ➞ -1
+
+
+// solution
+// 1. function search(arr, item) {
+// 	return arr.indexOf(item)
+// }
+// 2. const search=(arr,item)=>arr.indexOf(item)
+// 3. function search(arr,item) {
+//     return arr.findIndex(el=>el==item)
+// }
+// 4. const search=(arr,item)=>arr.findIndex(el=>el==item)
+// 5. function search(arr,item) {
+//     return arr.findLastIndex(el=>el==item)
+// }
+// 6. const search=(arr,item)=>arr.findLastIndex(el=>el==item)
+// 7. function search(arr, item) {
+// 	for(var i = 0;i<arr.length;i++){
+// 		if(arr[i] == item) return i;
+// 	}
+// 	return -1;
+// }
