d58912e766f54d846bbfd3d4e218a33e535f9b43
Navruz <navruz.halimov.3999@gmail.com>
2023-09-28 09:11:14 +0500

    14 problem fixed buggy code

--- array-very-easy.js
+++ array-very-easy.js
@@ -312,3 +312,34 @@
 // 	}
 // 	return -1;
 // }
+
+
+//################problem 14####################
+
+// Buggy Code (Part 3)
+// Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+// Examples
+// sumArray([1, 2, 3, 4, 5]) ➞ 15
+
+// sumArray([-1, 0, 1]) ➞ 0
+
+// sumArray([0, 4, 8, 12]) ➞ 24
+
+// function sumArray(arr) {
+// 	sum ,	
+//   for (i=0, i<arr, i++) {
+// 	  sum+=arr[i]
+// 	}
+//   return sum
+// }
+
+
+// solution
+function sumArray(arr) {
+	let sum=0;
+  for (let i=0; i<arr.length; i++) {
+	  sum+=arr[i]
+	}
+  return sum
+}
