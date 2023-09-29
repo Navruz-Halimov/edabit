661b1ead6f2f11fb4a1346227eb6ba9186c20239
Navruz <navruz.halimov.3999@gmail.com>
2023-09-29 09:21:55 +0500

    31 problem solved with 5 solution

--- array-very-easy.js
+++ array-very-easy.js
@@ -784,3 +784,34 @@
 //     return result
 // }
 
+
+// ############ 31 problem##########################
+// Difference of Volumes of Cuboids
+// Create a program that will take two arrays of integers, a and b. Each array will consist of 3 positive integers, representing the dimensions of cuboids a and b. Find the difference of the cuboids' volumes.
+
+// For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+
+// Examples
+// findDifference([ 28, 16, 29 ], [ 7, 8, 17 ]) ➞ 12040
+
+// findDifference([ 9, 22, 18 ], [ 16, 24, 10 ]) ➞ 276
+
+// findDifference([ 1, 9, 25 ], [ 10, 7, 9 ]) ➞ 405
+
+// findDifference([ 7, 6, 16 ], [ 26, 9, 26 ]) ➞ 5412
+
+
+// solution
+// 1. function findDifference(a, b) {
+// 	return Math.abs(a.reduce((el,acc)=>el*acc,1)-b.reduce((el,acc)=>el*acc,1))
+// }
+// 2. const findDifference=(a,b)=>Math.abs(a.reduce((el,acc)=>el*acc,1)-b.reduce((el,acc)=>el*acc,1))
+// 3.const findDifference =([a, b, c], [d, e, f]) => 
+// Math.abs(a * b * c - d * e * f);
+// 4. function findDifference(a, b) {
+// 	return Math.abs((a[0] * a[1] * a[2]) - (b[0] * b[1] * b[2]))
+// }
+// 5. const findDifference = (cube1, cube2) => {
+// 	const [[a, b, c], [d, e, f]] = [cube1, cube2];
+// 	return Math.abs(a*b*c - d*e*f);
+// }
