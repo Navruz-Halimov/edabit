e74ab77dda939d0232614fc1df7d64737afbb92a
Navruz <58542511+Navruz-Halimov@users.noreply.github.com>
2023-10-04 20:39:32 +0500

    41 problem solved with 4 solution

--- array-very-easy.js
+++ array-very-easy.js
@@ -1129,3 +1129,37 @@
 // 5. function skipTooMuchSugarDrinks(drinks) {
 // 	return drinks.filter(x => !/fanta|cola/.test(x))
 // }
+
+
+
+// #################### 41 problem################
+// Add the Index
+// Given an array of numbers, create a function which returns the same array but with each element's index in the array added to itself. This means you add 0 to the number at index 0, add 1 to the number at index 1, etc...
+
+// Examples
+// addIndexes([0, 0, 0, 0, 0]) ➞ [0, 1, 2, 3, 4]
+
+// addIndexes([1, 2, 3, 4, 5]) ➞ [1, 3, 5, 7, 9]
+
+// addIndexes([5, 4, 3, 2, 1]) ➞ [5, 5, 5, 5, 5]  
+
+// solution
+// 1. function addIndexes(arr) {
+// 	return arr.map((el,index)=>el+index)
+// }
+// 2. const addIndexes=arr=>arr.map((el,index)=>el+index)
+
+// 3. function addIndexes(arr){
+//     let result = []
+//     for(let i=0;i<arr.length; i++){
+//         result.push(arr[i]+i)
+//     }
+//     return result
+// }
+// 4. function addIndexes(arr){
+//     let result = []
+//     arr.forEach((element,index) => {
+//         result.push(element+index)
+//     });
+//     return result
+// }
