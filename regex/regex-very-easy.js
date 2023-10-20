36bb9924786295f66963d1746d742db31688d35f
Navruz <navruz.halimov.3999@gmail.com>
2023-10-20 22:44:04 +0500

    3 problem solved with 4 solution

--- regex/regex-very-easy.js
+++ regex/regex-very-easy.js
@@ -54,3 +54,34 @@
 // }
 // 4. const countSyllables = s =>s.match(/.{1,2}/g).length;
 // 5. const countSyllables = str => str.match(/[aeiou]/gi).length
+
+
+
+
+// ##################### problem 3 #############################
+// Amazing Edabit!
+// Create a function that takes a string and changes the word amazing to not amazing. Return the string without any change if the word edabit is part of the string.
+
+// Examples
+// amazingEdabit("edabit is amazing.") ➞ "edabit is amazing."
+
+// amazingEdabit("Mubashir is amazing.") ➞ "Mubashir is not amazing."
+
+// amazingEdabit("Infinity is amazing.") ➞ "Infinity is not amazing."
+
+
+// solution
+// 1. function amazingEdabit(str) {
+//     const regex = /amazing/gi;
+// 	if(str.includes('edabit')){
+//         return str
+//     }
+//     else{
+//         return str.replace(regex,'not amazing')
+//     }
+// }
+// 2. const amazingEdabit=str=>str.includes('edabit')?str:str.replace('amazing','not amazing')
+// 3. const amazingEdabit=s=>/edabit/.test(s) ? s : s.replace("is","is not")
+// 4. function amazingEdabit(str){
+//    return /edabit/.match(s) ? s : s.replace("is","is not")
+// }
