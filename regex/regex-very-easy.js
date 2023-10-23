331ad6f5fe92a3e7a0e75d5b36360afe8cebcb4d
Navruz <navruz.halimov.3999@gmail.com>
2023-10-23 10:15:15 +0500

    4 problem solved

--- regex/regex-very-easy.js
+++ regex/regex-very-easy.js
@@ -85,3 +85,20 @@
 // 4. function amazingEdabit(str){
 //    return /edabit/.match(s) ? s : s.replace("is","is not")
 // }
+
+
+// ################### problem 4#######################
+// Two Regular Expression Methods
+// There are three methods (we exclude compile) that you can use with regular expression literals. Use these two methods to fix the code. One method returns a boolean if there is a match. The other method returns an iterator from a search.
+
+// Notes
+// Do not use the same method twice.
+// Do not use String methods that accept regular expresssions as arguments.
+
+//  solution
+// function twoMethods() {
+// 	// find and equals are not regular expression methods.  Replace them. 
+// 	let methodOne = /hello/.exec("hello")[0] === "hello" 
+// 	let methodTwo = /hello/.test("hello") // returns a boolean
+// 	return methodOne && methodTwo
+// }
