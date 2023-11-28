b651b68884c439694a8ef6cccfb8959148324c14
Navruz <navruz.halimov.3999@gmail.com>
2023-11-28 20:46:46 +0500

    problem 4 solev with 6 solution

--- loop/easyloop.js
+++ loop/easyloop.js
@@ -97,3 +97,46 @@
 // 4. function calculateExponent(num, exp) {
 // 	return num ** exp
 // }
+
+
+// ############### problem 4###################
+
+// Find the Index
+// Create a function that takes an array and a string as arguments and returns the index of the string.
+
+// Examples
+// findIndex(["hi", "edabit", "fgh", "abc"], "fgh") ➞ 2
+
+// findIndex(["Red", "blue", "Blue", "Green"], "blue") ➞ 1
+
+// findIndex(["a", "g", "y", "d"], "d") ➞ 3
+
+// findIndex(["Pineapple", "Orange", "Grape", "Apple"], "Pineapple") ➞ 0
+// Notes
+// Don't forget to return the result.
+// If you are stuck, find help in the Resources tab.
+
+
+// solution
+// 1. function findIndex(arr, str) {
+// 	return arr.indexOf(str)
+// }
+// 2. const findIndex=(arr,str)=>arr.indexOf(str)
+// 3. function findIndex(arr,str){
+//     return arr.findIndex((element)=>element==str)
+// }
+// 4. const findIndex=(arr,str)=> arr.findIndex((element)=>element==str)
+// 5. function findIndex(arr,str){
+//     return arr.lastIndexOf((element)=>element==str)
+// }
+// 6. const findIndex=(arr,str)=> arr.lastIndexOf((element)=>element==str)
+// function findIndex(arr,str){
+// 	let indexEl
+// 	let x=arr.entries()
+//   for([index,el] of x){
+// 		if(el==str){
+// 			return index
+// 		}
+// 	}
+// 	return indexEl
+// }
